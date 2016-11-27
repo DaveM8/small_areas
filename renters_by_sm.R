812651f355f4682098bf2913239bfa99e3c8083d
davem8 <pizzadave108@gmail.com>
2016-11-27 00:37:11 +0000

    calculate percent of each renter type by small area

--- renters_by_sm.R
+++ renters_by_sm.R
@@ -8,9 +8,27 @@ source("read_api.R")
 ## T6_3_RLAH	Rented from Local Authority (Households)
 ## T6_3_RVBH	Rented from Voluntary Body (Households)
 ## T6_3_OFRH	Rented Free of Rent (Households)
-## T6_3_NSH	Not Stated (Households)
 ## T6_3_TH	Total (Households)
 
-sma <- read.csv("../small_areas/AllThemesTablesSA.csv")
-house_type <- sma %>% select(1:3,T6_3_RPLH)
-names(house_type)[4] <- tolower(gsub(" ", "_", "Households Rented from Private Landlord"))
+sma <- read.csv("AllThemesTablesSA.csv")
+house_type <- sma %>% select(1:3,T6_3_RPLH, T6_3_RLAH, T6_3_RVBH, T6_3_OFRH,T6_3_TH)
+field_names <- c("Households Rented from Private Landlord", "Households Rented from Local Authority", "Households Rented from Voluntary Body", "Households Rented Free of Rent", "Total Households")
+## total rented
+names(house_type)[4:8] <- tolower(gsub(" ", "_",field_names ))
+house_type$total_rented <- apply(house_type[,4:7], 1, sum)
+house_type <- house_type %>%
+    mutate(per_rented = total_rented/total_households)
+## break it down by private and local authority
+house_type <- house_type %>%
+    mutate(private_rented = households_rented_from_private_landlord/total_households)
+
+house_type <- house_type %>%
+    mutate(local_auth_rented = households_rented_from_local_authority/total_households)
+
+house_type <- house_type %>%
+    mutate(vol_body_rented = households_rented_from_voluntary_body/total_households)
+## write file to disk will load into postGIS to link GEOGID to
+## geographical location 
+write.table(house_type, file="rented_types_with_percent.csv", sep="\t", row.names=FALSE)
+
+
